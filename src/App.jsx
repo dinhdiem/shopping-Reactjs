81cc5d4620ff30f05c5055aa9fd4ac0158b7d10a
dinhdiem <miktendinh@gmail.com>
2022-07-27 11:44:13 +0700

    design Cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
-import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+// import Register from "./pages/Register";
 // import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 // import Product from "./pages/Product";
-// import Login from "./pages/Loing";
+// import Login from "./pages/login";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Cart />
     </div>
   );
 }
